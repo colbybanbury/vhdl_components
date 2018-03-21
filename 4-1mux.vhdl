c8deb655b6418ea21ad3da9d2d5a336712832336
colbybanbury <cbanbury@udel.edu>
2018-03-20 22:17:07 -0400

    mux is now of arbitrary size words and tb should be done

--- 4-1mux.vhdl
+++ 4-1mux.vhdl
@@ -1,21 +1,33 @@
 library ieee;
 use ieee.std_logic_1164.all;
 
+--4 to 1 multiplex
+--Authors: Colby Banbury and Matt Stout
+--takes the signals of 4 inputs and encodes them to a single output singnal
+--   using a 2 bit selector 
+
 entity mux is
-  port( A, B, C, D: in std_logic; --inputs
-        S0, S1 : in std_logic; --selectors
-             O : out std_logic);--outputs
+  generic(size : integer);
+
+  port( A    : in  std_logic_vector(size downto 0); --different inputs
+        B    : in  std_logic_vector(size downto 0); --Can be of arbitrary size
+        C    : in  std_logic_vector(size downto 0);
+        D    : in  std_logic_vector(size downto 0);
+        S    : in std_logic_vector(1 downto 0); --selectors
+        O : out std_logic_vector(size downto 0));--outputs
 end mux;
 
 architecture func of mux is
 begin
-  process (A,B,C,D,S0,S1) is
+  process (A,B,C,D,S) is
   begin
-    if ((S0='0') and (S1='0')) then
+--We chose to do more direct input output maping (instead of a more acurate circuit representation)
+--for the sake of clarity
+    if (S = "00") then
       O <= A;
-    elsif ((S0='1') and (S1='0')) then
+    elsif (S = "01") then
       O <= B;
-    elsif ((S0='0') and (S1='1')) then
+    elsif (S = "10") then
       O <= C;
     else
       O <= D;
---
+++ 4-1mux_tb.vhdl
@@ -0,0 +1,66 @@
+library ieee;
+use ieee.std_logic_1164.all;
+
+-- A testbench has no ports
+entity mux_tb is
+end mux_tb;
+
+architecture behavioral of mux_tb is
+-- Declaration of the component to be instantiated
+  component mux
+    generic(size : integer := 1);
+
+  port( A    : in  std_logic_vector(size downto 0); --different inputs
+        B    : in  std_logic_vector(size downto 0); --Can be of arbitrary size
+        C    : in  std_logic_vector(size downto 0);
+        D    : in  std_logic_vector(size downto 0);
+        S    : in std_logic_vector(1 downto 0); --selectors
+        O : out std_logic_vector(size downto 0));--outputs
+  end component;
+-- Specifies which entity is bound with the component
+  signal s                 : std_logic_vector(1 downto 0);
+  signal i0, i1, i2, i3, o : std_logic_vector(1 downto 0);
+begin
+-- Component instantiation
+  mux0 : mux_4_1 port map(S => s,
+                          A    => i0,
+                          B    => i1,
+                          C    => i2,
+                          D    => i3,
+                          O => o);
+-- This process does the real job
+  process
+    type pattern_type is record
+-- The inputs of the mux
+      i0, i1, i2, i3 : std_logic_vector(1 downto 0);
+      s              : std_logic_vector(1 downto 0);
+-- Expected outputs of mux      
+      o              : std_logic_vector(1 downto 0);
+    end record;
+-- The patterns to apply
+    type pattern_array is array (natural range <>) of pattern_type;
+    constant patterns : pattern_array :=
+      (("00", "01", "10", "11", "00", "00"),
+       ("00", "01", "10", "11", "01", "01"),
+       ("00", "01", "10", "11", "10", "10"),
+       ("00", "01", "10", "11", "11", "11"));
+  begin
+-- Check each pattern    
+    for n in patterns'range loop
+-- Set the inputs      
+      s  <= patterns(n).s;
+      i0 <= patterns(n).i0;
+      i1 <= patterns(n).i1;
+      i2 <= patterns(n).i2;
+      i3 <= patterns(n).i3;
+-- Wait for the result      
+      wait for 1 ns;
+-- Check the output      
+      assert o = patterns(n).o
+        report "bad output value" severity error;
+    end loop;
+      assert false report "end of test" severity note;
+-- Wait forever; this will finish the simulation
+    wait;
+  end process;
+end behavioral;
